4061c4d6c9aaa58dd8092cb94940cc5e7a0454f8
N <no@no.no>
2023-03-12 00:54:32 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
+import React from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+import * as Speech from 'expo-speech';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [ stringToSpeak, setString ] = React.useState ( '' );
+
+	return (
+		<View style={styles.container}>
+			<TextInput style={
+				{ 
+					fontSize: 18,
+					width: '50%',
+					borderColor: 'black', 
+					borderWidth: 2,
+					margin: 2
+				}
+				}
+				placeholder='Text'
+				value={ stringToSpeak }
+				onChangeText={ text => setString ( text ) }	
+			/>
+			<Button title='Speak' onPress={
+				async ( ) => {
+					let voiceEnglish = '';
+
+					for ( let voice of await Speech.getAvailableVoicesAsync ( ) ) {
+						if ( voice.language === 'en-GB' && voice.quality === 'Enhanced' ) {
+							voiceEnglish = voice.identifier;
+						}
+					}
+
+					Speech.speak ( stringToSpeak, { voice: voiceEnglish } );
+				}
+			}/>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
